3c97db75efbe801494750c96c177a56cb5e34311
Ebadullah Gul <ebadgul66@gmail.com>
2016-02-14 15:17:48 +0000

    deleting and upvoting is working now

--- public/javascripts/controllers/viewcoffeescontroler.js
+++ public/javascripts/controllers/viewcoffeescontroler.js
@@ -15,5 +15,30 @@ app.controller('viewCoffeesController', ['$scope','$http', function($scope, $htt
         });
 
     };
+
+    $scope.incrementUpvotes = function(id){
+        $http.put('/coffees/'+id+ '/votes')
+            .success(function(data){
+                console.log(data);
+                findAll();
+            }).error(function(data){
+            console.log('Error: '+data);
+        });
+    }
+
+
+    $scope.delete = function(id){
+        if(confirm("Are you sure you want to delete?")){
+            console.log('deleted coffee id: '+id);
+            $http.delete('/coffees/'+id)
+                .success(function(data){
+                    $scope.coffees = data;
+                    console.log(data);
+                    findAll();
+                }).error(function(data){
+                console.log('Error: '+data);
+            });
+        }
+    };
 }
 ]);
