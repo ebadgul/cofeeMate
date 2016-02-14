4b52b0b4f8b6552cac313a25d026ed33e51b061a
Ebadullah Gul <ebadgul66@gmail.com>
2016-02-14 05:13:39 +0000

    Done with adding and viewing coffees

--- public/javascripts/controllers/coffeecontroller.js
+++ public/javascripts/controllers/coffeecontroller.js
@@ -1,8 +1,31 @@
 var app = angular.module('coffeeMate');
 
-app.controller('coffeeController', ['$scope', function($scope){
-
+app.controller('coffeeController', ['$scope', '$location', '$http', function ($scope, $location, $http) {
     $scope.message = 'This ddafasdfa the Contact Page';
+
+    $scope.formData = {};
+
+    $scope.amount = 1000;
+    $scope.options = [{name: "PayPal", id: 0}, {name: "Direct", id: 1}];
+    $scope.formData.paymentOptions = $scope.options[0];
+
+    $scope.formData.paymenttype = 'PayPal';
+    $scope.formData.amount = 1000;
+    $scope.formData.upvotes = 0;
+
+    $scope.addCoffee = function () {
+        $scope.formData.paymenttype = $scope.formData.paymentOptions.name;
+        $http.post('/coffees', $scope.formData)
+            .success(function (data) {
+                $scope.coffees = data;
+                $location.path('/viewcoffees');
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
 }
 
 ]);
--- public/javascripts/controllers/viewcoffeescontroler.js
+++ public/javascripts/controllers/viewcoffeescontroler.js
@@ -1,8 +1,19 @@
 var app = angular.module('coffeeMate');
 
 
-app.controller('viewCoffeesController', ['$scope', function($scope) {
+app.controller('viewCoffeesController', ['$scope','$http', function($scope, $http) {
     // create a message to display in our view
     $scope.message = 'This viewing all coffe page';
+
+    findAll();
+    function findAll(){
+        $http.get('/coffees').success(function(data){
+            $scope.coffees = data;
+            console.log(data);
+            }).error(function(data){
+            console.log('Errorrr: '+data);
+        });
+
+    };
 }
 ]);
